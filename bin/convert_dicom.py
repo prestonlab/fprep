65891bce3da5d70538150b2abc97a7fcdfa55c2b
Neal Morton <mortonne@gmail.com>
2022-01-25 16:12:15 -0600

    style: black reformatting

--- bin/convert_dicom.py
+++ bin/convert_dicom.py
@@ -9,7 +9,7 @@ parser = SubjParser()
 args = parser.parse_args()
 
 sp = SubjPath(args.subject, args.study_dir)
-log = sp.init_log('dcm2nii', 'preproc', args)
+log = sp.init_log("dcm2nii", "preproc", args)
 
 # make sure standard directories exist
 sp.make_std_dirs()
