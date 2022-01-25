1a3852d4109c580df1948b5e19f2a4f280eb1212
Neal Morton <mortonne@gmail.com>
2022-01-25 14:38:53 -0600

    feat: add script to rename nifti files

---
+++ bin/rename_nifti.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from fprep.subjutil import *
+from fprep import heuristic
+
+parser = SubjParser()
+args = parser.parse_args()
+sp = SubjPath(args.subject, args.study_dir)
+log = sp.init_log('rename', 'preproc', args)
+
+log.start()
+heuristic.rename_bold(sp, log)
+heuristic.rename_anat(sp, log)
+log.finish()
